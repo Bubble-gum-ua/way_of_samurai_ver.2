8abfbb0a790f6f6bded407a19bfbb0278f093c33
bubble_gum_ua <bubble.gumgum1992@gmail.com>
2021-02-13 16:41:20 +0200

    Added "NavLink". Some styles.

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -3,15 +3,17 @@ import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import {makeStyles} from '@material-ui/core/styles';
+import {NavLink} from "react-router-dom";
+import "./Navigation.css"
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
-
     },
     paper: {
         marginRight: theme.spacing(2),
-        boxShadow: "none"
+        boxShadow: "none",
+
     },
 }));
 
@@ -21,11 +23,11 @@ export default function Navigation() {
     return (
         <div className={classes.root}>
             <Paper className={classes.paper}>
-                <MenuList>
-                    <MenuItem> <a href="/profile">My profile </a> </MenuItem>
-                    <MenuItem> <a href="/dialogs">Messages </a> </MenuItem>
-                    <MenuItem> <a href="/friends">Friends</a> </MenuItem>
-                    <MenuItem> <a href="/settings">Settings</a> </MenuItem>
+                <MenuList className="nav">
+                    <MenuItem> <NavLink to="/profile">My profile </NavLink> </MenuItem>
+                    <MenuItem> <NavLink to="/dialogs">Messages </NavLink> </MenuItem>
+                    <MenuItem> <NavLink to="/friends">Friends</NavLink> </MenuItem>
+                    <MenuItem> <NavLink to="/settings">Settings</NavLink> </MenuItem>
                 </MenuList>
             </Paper>
         </div>
